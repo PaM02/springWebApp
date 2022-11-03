13b781034ba5435075458f8e398ef8ceac4e6d69
papa matar fall <61315626+PaM02@users.noreply.github.com>
2022-11-03 23:09:54 +0000

    Initial commit

---
+++ src/main/java/com/openclassroms/webapp/webapp/repository/CustomProperties.java
@@ -0,0 +1,15 @@
+package com.openclassroms.webapp.webapp.repository;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+@ConfigurationProperties(prefix = "com.openclassrooms.webapp")
+@Data
+public class CustomProperties {
+
+    private String apiUrl;
+
+}
